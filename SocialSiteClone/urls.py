a16b6de221ae4b41245584538e9a69850576b586
nb9960 <nbodani8@gmail.com>
2020-08-03 21:51:03 +0530

    Login

--- SocialSiteClone/urls.py
+++ SocialSiteClone/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',views.HomePage.as_view(),name='home'),    
-    url(r'account/',include('account.urls',namespace='account')),
-    url(r'account/',include('django.contrib.auth.urls')),
-    url(r'^test/$',views.TestPage.as_view(),name='test'),
-    url(r'^thanks/$',views.ThanksPage.as_view(),name='thanks'),        
+    url(r'^account/',include('account.urls')), 
 ]
--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,6 @@
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
 from . import views
-
-app_name='account'
-
 urlpatterns = [
-    url(r'login/$',auth_views.LoginView.as_view(template_name='account/login.htm'), name='login'),
-    url(r'logout/$',auth_views.LogoutView.as_view(), name='logout'),
-    url(r'signup/$',views.SignUp.as_view(), name='signup'),
+    # post views
+    url(r'^login/', views.user_login, name='login'),
 ]
