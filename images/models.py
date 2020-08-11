d998ab61dbf36cc04c719660cd878a90223dd2e8
nb9960 <nbodani8@gmail.com>
2020-08-11 17:04:34 +0530

    Completed OAuth

--- images/models.py
+++ images/models.py
@@ -4,6 +4,7 @@ class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              related_name='images_created',
                              on_delete=models.CASCADE)
+     #use CASCADE for the on_delete parameter so that related images are also deleted when a user is deleted.
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200,
                             blank=True)
@@ -12,5 +13,15 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True,
                                db_index=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                    related_name='images_liked',
+                                    blank=True)
+                                    
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+      # slugify() function provided by Django to automatically generate the image slug for the given title when no slug is provided
