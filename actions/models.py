08f25b1aee33cff3887041a3e9c93f658bd5aca9
nb9960 <nbodani8@gmail.com>
2020-09-13 23:32:06 +0530

    modified action model

--- actions/models.py
+++ actions/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+# A field to define and manage the generic relation using the foreignkey to contenttype, primary key
 class Action(models.Model):
     user = models.ForeignKey('auth.User',
                              related_name='actions',
                              db_index=True,
                              on_delete=models.CASCADE)
     verb = models.CharField(max_length=255)
+    # A ForeignKey field that points to the ContentType model
     target_ct = models.ForeignKey(ContentType,
                                   blank=True,
                                   null=True,
                                   related_name='target_obj',
                                   on_delete=models.CASCADE)
+    # A PositiveIntegerField for storing the primary key of the related object
     target_id = models.PositiveIntegerField(null=True,
                                             blank=True,
                                             db_index=True)
+    # A GenericForeignKey field to the related object based on the combination of the two previous fields
+    # Django doesn't create any field in the database for GenericForeignKey fields
     target = GenericForeignKey('target_ct', 'target_id')
     created = models.DateTimeField(auto_now_add=True,
                                    db_index=True)
